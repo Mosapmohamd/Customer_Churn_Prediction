013177683d4188501c3ff4ea43f5a5c441c162df
Mosap Abdel-Ghany <abdelghanymosap@gmail.com>
2025-06-03 16:01:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -55,9 +55,10 @@ def load_sample_profile(profile_type):
             'PaymentMethod': "Bank transfer (automatic)",
             'MonthlyCharges': 50.0,
             'TotalCharges': 3000.0
-             for key, value in sample_data.items():
-                st.session_state[key] = value
-            st.experimental_rerun()       }
+       }
+        for key, value in sample_data.items():
+            st.session_state[key] = value
+        st.experimental_rerun()   
     else:  # high_risk
         return {
             'Gender': "Male",
@@ -79,10 +80,10 @@ def load_sample_profile(profile_type):
             'PaymentMethod': "Electronic check",
             'MonthlyCharges': 100.0,
             'TotalCharges': 100.0
-            for key, value in sample_data.items():
-                st.session_state[key] = value
-            st.experimental_rerun()   }
-
+               }
+        for key, value in sample_data.items():
+            st.session_state[key] = value
+        st.experimental_rerun()   
 # App header
 col1, col2 = st.columns([1, 3])
 with col1:
